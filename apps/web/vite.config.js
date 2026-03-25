37a59fd05903947c1dd37ce80c4ba6848a8bc4fb
Ravindra Nag <ravindranag52@gmail.com>
2026-03-25 23:20:42 +0530

    feat (web): install pandacss

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import path from 'node:path'
+
+const __dirname = path.dirname(import.meta.url)
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +11,13 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  resolve: {
+    alias: {
+      '@/styled-system': path.join(__dirname, 'styled-system'),
+      '@': path.join(__dirname, 'src')
+    }
+  },
+  server: {
+    port: 3000
+  }
 })
